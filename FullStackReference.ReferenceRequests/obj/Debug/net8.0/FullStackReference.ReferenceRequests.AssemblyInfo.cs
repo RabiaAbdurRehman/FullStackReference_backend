82878fa8f608c45218d5f9092d8bc2b180e09100
Rabia <rabia.abdur.rehman@gmail.com>
2024-03-11 09:21:38 -0600

    Add file

--- FullStackReference.ReferenceRequests/obj/Debug/net8.0/FullStackReference.ReferenceRequests.AssemblyInfo.cs
+++ FullStackReference.ReferenceRequests/obj/Debug/net8.0/FullStackReference.ReferenceRequests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullStackReference.ReferenceRequests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5710e9c2173f08805652c928853b083aca7bafad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6140a80f254496d8dbadfdb35b9fc0cf48a28ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullStackReference.ReferenceRequests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullStackReference.ReferenceRequests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
